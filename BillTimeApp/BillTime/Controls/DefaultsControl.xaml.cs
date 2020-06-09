b954ae05e9afc94c7d564033995e3abc3631c3e7
Ivacks <ivan.navarro.vargas@gmail.com>
2020-06-09 13:23:17 +0200

    First but not the last

---
+++ BillTimeApp/BillTime/Controls/DefaultsControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using BillTimeLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BillTime.Controls
+{
+    /// <summary>
+    /// Interaction logic for DefaultsControl.xaml
+    /// </summary>
+    public partial class DefaultsControl : UserControl
+    {
+        public DefaultsControl()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadDefaultsFromDatabase()
+        {
+            DefaultsModel 
+        }
+    }
+}
---
+++ BillTimeApp/BillTimeLibrary/DataAccess/SqliteDataAccess.cs
@@ -0,0 +1,61 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillTimeLibrary.DataAccess
+{
+    public static class SqliteDataAccess
+    {
+
+        // LoadData<PersonModel>("Select * from Person", null) = List<PersonModel>
+        public static List<T> LoadData<T>(string sqlStatement, Dictionary<string, object> parameters, string dbName = "Default")
+        {
+            DynamicParameters p = parameters.ToDynamicParameters();
+
+            //parameters.ToList().ForEach(x => p.Add(x.Key, x.Value));
+
+            // Two Options //
+            //foreach(var param in parameters)
+            //{
+            //    //    @FirstName, "Tim"
+            //    p.Add(param.Key, param.Value);
+            //}
+
+            using (IDbConnection cnn = new SQLiteConnection(DataAccessHelpers.LoadConnectionString(dbName)))
+            {
+                var rows = cnn.Query<T>(sqlStatement, p);
+
+                return rows.ToList();
+            }
+        }
+
+        public static void SaveData(string sqlStatement, Dictionary<string, object> parameters, string connectionName = "Default")
+        {
+            DynamicParameters p = parameters.ToDynamicParameters();
+
+            //parameters.ToList().ForEach(x => p.Add(x.Key, x.Value));
+
+            using (IDbConnection cnn = new SQLiteConnection(DataAccessHelpers.LoadConnectionString(connectionName))) 
+            {
+                cnn.Execute(sqlStatement, p);
+            }
+        }
+
+
+        private static DynamicParameters ToDynamicParameters(this Dictionary<string, object> p)
+        {
+            DynamicParameters output = new DynamicParameters();
+
+            p.ToList().ForEach(x => output.Add(x.Key, x.Value));
+
+            return output;
+        }
+
+
+    }
+}
